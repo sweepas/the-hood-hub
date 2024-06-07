22b9d9af24044d69ec2417f5e5f77ee2fccdd2da
sweepas <47362063+sweepas@users.noreply.github.com>
2024-06-07 09:42:45 +0100

    clerk setup

---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+const MobileNav = () => {
+  return (
+    <nav className="">
+      <Sheet>
+        <SheetTrigger className="bg-blue-500 p-2 text-black rounded">
+          Openevwfvffwrvervfr4tvtv
+        </SheetTrigger>
+        <SheetContent side="right">
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
---
+++ components/shared/NavItems.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavItems = () => {
+  return (
+    <div className="bg-green-900">
+      omg
+    </div>
+  );
+};
+
+export default NavItems;
